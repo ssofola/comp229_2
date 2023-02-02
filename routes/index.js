a00153a8a4dbd8677922ef3bbb8d54bd7122ce63
Olusegun Sofola <ssofola@gmail.com>
2023-02-02 06:51:02 -0500

    updated code with assignment items

--- routes/index.js
+++ routes/index.js
@@ -12,22 +12,22 @@ router.get('/home', function(req, res, next) {
 
 /* GET About Me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET home page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
