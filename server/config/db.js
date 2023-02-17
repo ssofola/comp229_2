8869e00e48b2e5ca42663368278b3e0d55be2c38
Olusegun Sofola <ssofola@gmail.com>
2023-02-17 10:59:44 -0500

    updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@
  //         Date: February 3, 2023
  *****************************************/
 module.exports = {
-    "URI":"mongodb+srv://osofola1:vxznlrOWNLhuMmEj@cluster0.ll6n0o1.mongodb.net/?retryWrites=true&w=majority"
+    "URI":"mongodb+srv://osofola1:vxznlrOWNLhuMmEj@cluster0.ll6n0o1.mongodb.net/OlusegunPersonal?retryWrites=true&w=majority"
 }
 
 // Local URI - "URI":"mongodb://192.168.56.2:27017/bookstore"
