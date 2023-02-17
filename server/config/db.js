e5a2f9b122964ab25f8b42a4a47296011a4707a3
Olusegun Sofola <ssofola@gmail.com>
2023-02-17 10:30:08 -0500

    updated with the latest db config for atlas

--- server/config/db.js
+++ server/config/db.js
@@ -6,5 +6,7 @@
  //         Date: February 3, 2023
  *****************************************/
 module.exports = {
-    "URI":"mongodb://192.168.56.2:27017/bookstore"
+    "URI":"mongodb+srv://osofola1:vxznlrOWNLhuMmEj@cluster0.ll6n0o1.mongodb.net/?retryWrites=true&w=majority"
 }
+
+// Local URI - "URI":"mongodb://192.168.56.2:27017/bookstore"
